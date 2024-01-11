425c7e9c56563a08ba5171075bcded9f871f0ea9
A-Nickgithub <nicholusalbertmirindo.com>
2024-01-11 11:58:38 +0300

    0 update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    m = []
-    for le in matrix:
-        m.append(list(map(lambda x: x * x, le)))
-        return m
+    """
+    Computes the square value of all integers in a matrix.
+    Args:
+    matrix (list of list): A 2-dimensional array.
+    Returns:
+    list of list: A new matrix with each value being the square of the input.
+    """
+    new_matrix = []
+
+    for row in matrix:
+        new_row = [x**2 for x in row]
+        new_matrix.append(new_row)
+
+        return new_matrix
