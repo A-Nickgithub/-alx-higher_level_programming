ebe566a3f6539cc4c24aa9ddcb18e5c17fc23d8d
A-Nickgithub <nicholusalbertmirindo.com>
2024-02-05 17:25:20 +0300

    update

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This modules tells whether an object is an exact instance
+of a class
+"""
+
 
 def is_same_class(obj, a_class):
     """Returns True if the object is exactly an instance of
