ff12457007c4cd1708af97fbf99291e4932f9185
A-Nickgithub <nicholusalbertmirindo.com>
2024-02-05 17:27:32 +0300

    update

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module tells us whether an object is an
+instance of a class or an instance of a derived class
+"""
+
 
 def is_kind_of_class(obj, a_class):
     """Returns True if the object is an instance of,
