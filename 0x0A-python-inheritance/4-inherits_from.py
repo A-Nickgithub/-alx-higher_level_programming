a013075509daf15182b9063144e5c56f95a03d47
A-Nickgithub <nicholusalbertmirindo.com>
2024-02-05 17:29:25 +0300

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module checks whether an object is an instance of
+a class that inherited directly or indirectly from a
+specified class
+"""
+
 
 def inherits_from(obj, a_class):
     """Returns True if the object is an instance of a
