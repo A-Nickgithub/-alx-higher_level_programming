12bf7a0267bee5648db7e47943c4ab229d494194
A-Nickgithub <nicholusalbertmirindo.com>
2024-02-05 17:23:09 +0300

    update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+This module shows a class MyList tha inherit from
+the well known list class
+so it will inherit all the public methods and attributes such as append
+It also inherits the sort methos
+"""
+
 
 class MyList(list):
     """Inherits from list and adds a method to
