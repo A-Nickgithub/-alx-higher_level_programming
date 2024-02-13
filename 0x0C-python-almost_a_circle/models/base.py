388d99e2603cae076635031713a2ff296b147c19
A-Nickgithub <nicholusalbertmirindo.com>
2024-02-13 04:11:45 +0300

    updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,12 +16,11 @@ class Base:
         Args:
         id(int) = id attribute
         """
-
-    if id is None:
-        Base.__nb_objects += 1
-        id = Base.__nb_objects
-
-        self.id = id
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
@@ -37,15 +36,15 @@ class Base:
 
         """
 
-    filename = cls.__name__ + ".json"
+        filename = cls.__name__ + ".json"
 
-    with open(filename, "w") as jasonfile:
-        if list_objs is None:
-            jasonfile.write("[]")
+        with open(filename, "w") as jasonfile:
+            if list_objs is None:
+                jasonfile.write("[]")
 
-        else:
-            list_dicts = [o.to_dictionary() for o in list_objs]
-            jasonfile.write(Base.to_json_string(list_dicts))
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jasonfile.write(Base.to_json_string(list_dicts))
 
     @staticmethod
     def from_json_string(json_string):
@@ -75,7 +74,7 @@ class Base:
 
         else:
 
-            dummy = clas(1)
+            dummy = cls(1)
 
             dummy.update(**dictionary)
 
@@ -88,16 +87,17 @@ class Base:
         return list of instance
         """
 
-    filename = cls.__name__ + '.json'
-    new = []
+        filename = cls.__name__ + '.json'
+        new = []
 
-    try:
-        with open(filename) as f:\
+        try:
+            with open(filename) as f:
                 content = f.read()
 
-    except Exception as e:
-        return new
-    json_file = Base.from_json_string(content)
-    for obj in json_file:
-        new.append(cls.create(**obj))
-        return new
+        except Exception as e:
+            return new
+
+        json_file = Base.from_json_string(content)
+        for obj in json_file:
+            new.append(cls.create(**obj))
+            return new
