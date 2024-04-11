91773dac5128f9dab3b8715a623aa52ae0d2cfa8
root <root@DESKTOP-NHD29B4.localdomain>
2024-04-11 21:39:02 +0300

    3

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,28 +9,23 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database = sys.argv[1:]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
         user=username,
-        passwd=password,
-        db=database
+        password=password,
+        database=database,
+        port=3306
         )
 
-    cursor = db.cursor()
+    cur = db.cursor()
+    cur.execute("SELECT DISTINCT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_results = cur.fetchall()
+    for row in query_results:
+        print(row)
 
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
-
-    cursor.close()
-    db.close()
+    cur.close()
