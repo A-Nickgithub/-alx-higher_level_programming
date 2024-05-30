ca0d9a8ffbca27cf74a256201756d8b29e4c434d
nicholusalbert <nicholusalbertmirindo@gmail.com>
2024-05-31 00:56:21 +0300

    web jquery

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+/* global $ */
+
+$(document).ready(function () {
+  $('#toggle_header').click(function () {
+    if ($('header').hasClass('red')) {
+      $('header').removeClass('red').addClass('green');
+    } else {
+      $('header').removeClass('green').addClass('red');
+    }
+  });
+});
